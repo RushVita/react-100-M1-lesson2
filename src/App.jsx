c4b875fa9ab6bd236ef1205b9a9fdfb7375e6b36
Vitaliy Parhomovych <rushvita1991@gmail.com>
2024-04-09 19:02:57 +0300

    initial-project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro quia
+        odio nam alias. Debitis, quo doloribus numquam dolorum sequi commodi
+        esse laboriosam voluptas officiis, maxime repudiandae mollitia ex nobis
+        excepturi? Tempora, aspernatur? Officia commodi provident quia ipsa, id
+        totam quo?
+      </p>
+    </div>
+  );
+}
+
+export default App;
